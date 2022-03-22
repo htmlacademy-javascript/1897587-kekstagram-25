aa03ba9aa967d0331f72b520ca3ad6a414e54aef
AlexanderZamazkin <Alex_zam_2000@mail.ru>
2022-03-22 19:16:26 +0300

    Нужно больше функций(испр)

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 const returning = function (from, to) {
   if (to<from) {
-    console.log('Диапазон от',from,'до',to,'указан неверно');
+    return(false);
   }
   if (from>=0 && to>=0 && from<to) {
     return(Math.floor(Math.random() * (to - from + 1) + from));
   } else if (from === to){
-    console.log('Данные числа не являются диапазоном, это одно число',from);
+    return(false);
   }
 };
 
-console.log(returning(5,10));
+returning(5,10);
 
 const check = function (str, lngt) {
   if (str.length>lngt) {
-    console.log('Длина введённого комментария не может быть больше', lngt, 'символов');
+    return(false);
   } else if (str.length<=lngt) {
-    console.log('Комментарий успешно введён');
+    return(true);
   }
 };
 
-console.log(check('dsadsadasddsdsa',10));
+check('dsafds',10);
