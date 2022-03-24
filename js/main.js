7952c97684a9b68efb6a1031c4a818e95a89567d
AlexanderZamazkin <Alex_zam_2000@mail.ru>
2022-03-24 13:18:17 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
-const returning = function (from, to) {
-  if (to<from) {
-    return(false);
-  }
+const returning = (from, to) => {
   if (from>=0 && to>=0 && from<to) {
-    return(Math.floor(Math.random() * (to - from + 1) + from));
-  } else if (from === to){
-    return(false);
+    return Math.floor(Math.random() * (to - from + 1) + from);
   }
+  return -1;
 };
-
 returning(10,12);
 
-const check = function (str, lngt) {
+const check = (str, lngt) => {
   if (str.length>lngt) {
-    return(false);
-  } else if (str.length<=lngt) {
-    return(true);
+    return false;
   }
+  return true;
 };
-
 check('dasfsa',10);
+
